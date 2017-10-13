46b6ed254796e2e57d6337342682cfd03ba83fc8
sososan <574653854@qq.com>
2017-10-13 22:28:56 +0800

    123

--- js/index.js
+++ js/index.js
@@ -30,4 +30,65 @@ $(function(){
 	$(".backtop").backTop();
 	//$(".backtop") 返回按钮父容器     
 
+
+
+	$(".icon").click(function () {
+	        $({dNum:0}).animate({dNum:650},{
+	            duration:850,
+	            step: function () {
+	                $(window).scrollTop(this.dNum);
+	            }
+	        })
+	    });
+	// 上面的这堆代码的意思是点击后向下滚动，用的时候需要更改的地方
+	// 是 ".icon" 需要编程属性名，0 和 700 的意思是从 0 移动到 700 ，
+	// 后面的 850 表示经历的时间为 850 毫秒
+
+	$("nav .nav li:eq(0)").click(function () {
+	    $({num:$(window).scrollTop()}).animate({num:0},{
+	        duration:850,
+	        step: function () {
+	                $(window).scrollTop(this.num);
+	            }
+	        })
+	    });
+
+	$("nav .nav li:eq(1)").click(function () {
+	    $({num:$(window).scrollTop()}).animate({num:650},{
+	        duration:850,
+	        step: function () {
+	                $(window).scrollTop(this.num);
+	            }
+	        })
+	    });
+	$("nav .nav li:eq(2)").click(function () {
+	    $({num:$(window).scrollTop()}).animate({num:1300},{
+	        duration:850,
+	        step: function () {
+	                $(window).scrollTop(this.num);
+	            }
+	        })
+	    });
+	$("nav .nav li:eq(3)").click(function () {
+	    $({num:$(window).scrollTop()}).animate({num:1995},{
+	        duration:850,
+	        step: function () {
+	                $(window).scrollTop(this.num);
+	            }
+	        })
+	    });
+	$("nav .nav li:eq(4)").click(function () {
+	    $({num:$(window).scrollTop()}).animate({num:3500},{
+	        duration:850,
+	        step: function () {
+	                $(window).scrollTop(this.num);
+	            }
+	        })
+	    });
+
+	// 上面的这一堆表示从导航栏里点击移动页面到哪一屏，黄色的表示
+	// 类名选择器，eq(0)表示选择 li 的第一个，num$(window)表示从当
+	// 前屏开始移动，后面变化的数字表示移动到哪一位置，后面的 850
+	// 表示移动花费的时间
+	
 })
